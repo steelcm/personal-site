fae1727947c48d2174cdf49cd6fa2cbf68dae2f6
steelcm <csteel@gmail.com>
2018-01-22 15:53:14 +0000

    Update page structure and theme

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,8 +2,21 @@ import React from "react"
 import PropTypes from "prop-types"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import SvgIcon from 'material-ui/SvgIcon';
 
-import "../css/typography.css"
+import {
+  lightBlue600,
+} from 'material-ui/styles/colors';
+
+
+
+
+import "./index.scss"
 
 export default class Template extends React.Component {
   static propTypes = {
@@ -12,51 +25,20 @@ export default class Template extends React.Component {
 
   render() {
     return (
-      <div>
-        <Helmet
-          title="Gatsby Default Starter"
-          meta={[
-            { name: "description", content: "Sample" },
-            { name: "keywords", content: "sample, something" },
-          ]}
-        />
-        <div
-          style={{
-            background: `rebeccapurple`,
-            marginBottom: `1.45rem`,
-          }}
-        >
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `1.45rem 1.0875rem`,
-            }}
+      <MuiThemeProvider>
+        <div>
+          <Helmet
+            title="Panda Bridge"
+            meta={[
+              { name: "description", content: "Software development agency and consultancy based in London, UK" },
+              { name: "keywords", content: "software consultance, software development, freelance, contracting" },
+            ]}
           >
-            <h1 style={{ margin: 0 }}>
-              <Link
-                to="/"
-                style={{
-                  color: "white",
-                  textDecoration: "none",
-                }}
-              >
-                Gatsby
-              </Link>
-            </h1>
-          </div>
-        </div>
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          </Helmet>
           {this.props.children()}
         </div>
-      </div>
+      </MuiThemeProvider>
     )
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,64 @@
 import React from "react";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
+import Avatar from 'material-ui/Avatar';
+import FontIcon from 'material-ui/FontIcon';
+import {
+  blue300,
+  indigo900,
+  orange200,
+  deepOrange300,
+  pink400,
+  purple500,
+  lightBlue600
+} from 'material-ui/styles/colors';
+
+import "../css/_index.scss"
+
+const style = { margin: 5 };
+const iconStyles = {
+  marginRight: 24,
+};
 
 export default class Index extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hi people</h1>
-        <p>
-          This is an example integrating Netlify’s form handling with Gatsby
-        </p>
-        <Link to="/contact/">Go to contact form</Link>
+        <section id="intro">
+          <div className="container">
+            <div className="half">
+              <h1>Panda Bridge</h1>
+              <h2>Panda Bridge is Christopher Steel, a London based freelance web developer &amp; consultant.</h2>
+              <p>Vestibulum interdum augue quis massa ornare, vel posuere lectus interdum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus eros libero, aliquam pharetra sagittis id, dignissim in ipsum. Aenean et volutpat nisl. Cras et massa vel orci efficitur facilisis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam non purus lorem. Donec ante libero, consectetur ut interdum sed, consectetur id dolor. Ut ultrices gravida ligula. Nunc bibendum ex id est blandit finibus.</p>
+              <a className="button" href="#contact">Get In Contact</a>
+            </div>
+          </div>
+        </section>
+        <section id="about">
+          <div className="container">
+            <div className="padded center">
+              <h3 className="underline">Who is it for?</h3>
+              <p>Go Freelance has been written for digital and print creators - from designers and illustrators to front-end and back-end developers. If you've spent (or plan on spending) any time working in the digital creative industry then it should be a pretty good fit for you.</p>
+            </div>
+            <div className="padded center">
+              <h3 className="underline">Who is it for?</h3>
+              <p>Go Freelance has been written for digital and print creators - from designers and illustrators to front-end and back-end developers. If you've spent (or plan on spending) any time working in the digital creative industry then it should be a pretty good fit for you.</p>
+            </div>
+            <div className="padded center">
+              <h3 className="underline">Who is it for?</h3>
+              <p>Go Freelance has been written for digital and print creators - from designers and illustrators to front-end and back-end developers. If you've spent (or plan on spending) any time working in the digital creative industry then it should be a pretty good fit for you.</p>
+            </div>
+          </div>
+        </section>
+        <section id="quote">
+          <div className="container center">
+            <p className="underline">Go Freelance has been written for digital and print creators - from designers and illustrators to front-end and back-end developers. If you've spent (or plan on spending) any time working in the digital creative industry then it should be a pretty good fit for you.</p>
+            <b> 
+              James Browning<br/>
+              <a href="#">Gloucestershire University</a>
+            </b>
+          </div>
+        </section>
       </div>
     );
   }
